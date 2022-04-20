8f8e29c59983cba491f88cfc32d49b8c248bc268
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2022-04-21 03:10:13 +1000

    completed EFA

--- EFA/efa.py
+++ EFA/efa.py
@@ -1,9 +1,9 @@
 # Creation Date: 18/04/2022
 
 
+import os
 import numpy
 import pandas
-import itertools
 import matplotlib
 import matplotlib.pyplot
 from factor_analyzer import FactorAnalyzer
@@ -12,7 +12,18 @@ from factor_analyzer.factor_analyzer import calculate_bartlett_sphericity, calcu
 
 # Matplot setup
 matplotlib.rcParams['figure.figsize'] = (10.0, 6.0)
-# matplotlib.use('Agg')
+
+
+def prepare_log(datafile: str, log: str):
+    if log == None:
+        reference = f"EFA/{datafile}.txt"
+    else:
+        reference = f"EFA/{log}.txt"
+    try:
+        os.remove(reference)
+    except OSError:
+        pass
+    return open(reference, "a")
 
 
 def base26(n):
@@ -37,13 +48,14 @@ def load_CSV(filename: str, drop_list: list):
     return dataframe
 
 
-def adequacy_test(data):
+def adequacy_test(data, log):
     ''' Purpose: Measures adequacy of data for factorability. '''
+    print(f"{'='*75}\nFactorability testing\n", file=log)
     chi_square_score, p_value = calculate_bartlett_sphericity(
         data)  # Bartlett's Test
     _, kmo_score = calculate_kmo(data)  # Kaiser-Meyer-Olkin Test
-    print(f"Bartlett's Test: {(chi_square_score, p_value)}")
-    print(f"Kaiser-Meyer-Olkin Test: {(kmo_score)}")
+    print(f"Bartlett's Test: {(chi_square_score, p_value)}", file=log)
+    print(f"Kaiser-Meyer-Olkin Test: ({kmo_score})", file=log)
 
 
 def plot_eisen_values(filename: str, eigen_values: list, width: int):
@@ -78,10 +90,11 @@ def plot_horn_results(filename: str, eigenvalues: list, average_paf_eigens: list
     matplotlib.pyplot.savefig(f"EFA/{filename}_parallel_analysis.png")
 
 
-def horn_parallel_analysis(data, filename: str, times: int = 100):
+def horn_parallel_analysis(data, filename: str, log: str, times: int = 100):
     ''' Purpose: Performs horn parallel analysis to determine topics. '''
+    print(f"\n{'='*75}\nHorn parallel analysis\n", file=log)
     height, width = data.shape
-    paf = FactorAnalyzer(n_factors=1, method='principal', rotation=None)
+    paf = FactorAnalyzer(n_factors=11, method='principal', rotation=None)
     sum_paf_eigens = numpy.empty(width)
     for _ in range(0, times):
         paf.fit(numpy.random.normal(size=(height, width)))
@@ -89,12 +102,11 @@ def horn_parallel_analysis(data, filename: str, times: int = 100):
     average_paf_eigens = sum_paf_eigens / times
     paf.fit(data)
     eigenvalues = paf.get_eigenvalues()[0]
-    print(f"Factor eigenvalues for random matrix:\n{average_paf_eigens}")
-    print('Factor eigenvalues for data:\n', eigenvalues)
-    suggestedFactors = sum((eigenvalues - average_paf_eigens) > 0)
-    print(suggestedFactors)
-    print("here")
-    print(average_paf_eigens)
+    print(
+        f"Factor eigenvalues for random matrix:\n{average_paf_eigens}\n", file=log)
+    print(f"Factor eigenvalues for data:\n{eigenvalues}\n", file=log)
+    suggested = sum((eigenvalues - average_paf_eigens) > 0)
+    print(f"Parallel analysis suggested factors:{suggested}\n", file=log)
     plot_horn_results(filename, eigenvalues, average_paf_eigens, width)
 
 
@@ -110,22 +122,40 @@ def plot_scree(proportional_variance: list, filename: str, width: int):
     matplotlib.pyplot.savefig(f"EFA/{filename}_variance_scree.png")
 
 
-def determine_topics(data, filename: str, eisenvalue: bool, parallel: bool):
+def determine_topics(data, total_topics: int, filename: str, eisenvalue: bool,
+                     parallel: bool, log: str):
     ''' Purpose: Analysis results to determine topic number. '''
-    adequacy_test(data)
+    adequacy_test(data, log)
     _, width = data.shape
-    paf = FactorAnalyzer(n_factors=11, method='principal', rotation=None)
+    paf = FactorAnalyzer(n_factors=total_topics,
+                         method='principal', rotation=None)
     paf.fit(data)
     if eisenvalue:
         eigen_values = paf.get_eigenvalues()[0]
+        print(f"\n{'='*75}\nEigenvalue analysis\n", file=log)
+        print(f"Eigenvalues:\n{eigen_values}\n", file=log)
+        suggested = next(x[0] for x in enumerate(eigen_values) if x[1] < 1)
+        print(
+            f"Eigenvalue suggested factors: {suggested}", file=log)
         plot_eisen_values(filename, eigen_values, width)
     if parallel:
-        horn_parallel_analysis(data, filename)
-    summary = paf.get_factor_variance()
-    print(f"Variance: {summary[0]}")
-    print(f"Proportional Variance: {summary[1]}")
-    print(f"Cumulative Variance: {summary[2]}")
-    plot_scree(summary[1], filename, width)
+        horn_parallel_analysis(data, filename, log)
+    variance = paf.get_factor_variance()
+    print(f"Variance\n{variance[0]}\n", file=log)
+    print(f"Proportional variance\n{variance[1]}\n", file=log)
+    print(f"Cumulative variance\n{variance[2]}", file=log)
+    plot_scree(variance[1], filename, width)
+
+
+def user_input():
+    ''' Returns: User entered number of topics integer. '''
+    while True:
+        try:
+            topic_count = int(input("Number of topics:"))
+            break
+        except:
+            print("Enter a valid integer!")
+    return topic_count
 
 
 def plot_paf_results(paf, filename: str, number_topics: int, headers: list):
@@ -137,6 +167,7 @@ def plot_paf_results(paf, filename: str, number_topics: int, headers: list):
     figure.colorbar(x, ax=axis)
     axis.set_yticks(numpy.arange(paf.loadings_.shape[0])+0.5, minor=False)
     axis.set_xticks(numpy.arange(paf.loadings_.shape[1])+0.5, minor=False)
+    axis.set_title("EFA Results")
     axis.set_yticklabels(headers)
     axis.set_xticklabels(xlabels)
     figure.tight_layout()
@@ -144,28 +175,31 @@ def plot_paf_results(paf, filename: str, number_topics: int, headers: list):
 
 
 # Rotation methods: https://factor-analyzer.readthedocs.io/en/latest/
-def EFA_analysis(data, filename: str, number_topics: int, rotation_method: str):
+def EFA_analysis(data, filename: str, number_topics: int, rotation_method: str, log: str):
     ''' Purpose: Complete Exploratory Factor Analysis. '''
     headers = list(data.columns.values)
     paf = FactorAnalyzer()
     paf.set_params(n_factors=number_topics, rotation=rotation_method)
     paf.fit(data)
     plot_paf_results(paf, filename, number_topics, headers)
-    print(paf.get_factor_variance())
-    print(f"Correlation Scores: \n{paf.loadings_}")
+    print(f"Correlation Scores:\n{paf.loadings_}\n", file=log)
 
 
-def EFA_pipeline(datafile: str, topic_list: list = [1], rotation_list: list = ["oblimin"],
-                 eisenvalue: bool = True, parallel: bool = True, drop_list: list = None):
+def EFA_pipeline(datafile: str, rotation_list: list = ["oblimin"], eisenvalue: bool = True,
+                 parallel: bool = True, drop_list: list = None, log: str = None):
     ''' Purpose: Completes PAF based EFA given provided arguments. '''
+    LOG = prepare_log(datafile, log)
+    print(LOG)
     data = load_CSV(datafile, drop_list)
-    determine_topics(data, f"EFA_{datafile}", eisenvalue, parallel)
-    for topic_count, rotation in itertools.product(topic_list, rotation_list):
-        EFA_analysis(data, f"EFA_{datafile}_{rotation}", topic_count, rotation)
+    total_topics = len(list(data.columns.values))
+    determine_topics(data, total_topics,
+                     f"EFA_{datafile}", eisenvalue, parallel, LOG)
+    topic_count = user_input()
+    print(f"\n{'='*75}\nEFA\n\nTopic Number: {topic_count}\n", file=LOG)
+    for rotation in rotation_list:
+        print(f"EFA Rotation: {rotation}\n", file=LOG)
+        EFA_analysis(
+            data, f"EFA_{datafile}_{rotation}_{topic_count}", topic_count, rotation, LOG)
 
 
-EFA_pipeline("data", topic_list=[3], drop_list=["subno"])
-
-topic_options = [2, 3]
-rotation_options = ["oblimin", "promax"]
-#EFA_pipeline(topic_options, rotation_options)
+EFA_pipeline("data", drop_list=["subno"], rotation_list=["oblimin", "promax"])
